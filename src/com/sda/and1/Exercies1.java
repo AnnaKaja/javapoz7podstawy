3dd02128c4424d7cc14110d911788beadd48201a
anna kaja <anna_kaja@op.pl>
2018-03-16 20:14:52 +0100

    exercise

---
+++ src/com/sda/and1/Exercies1.java
@@ -0,0 +1,28 @@
+package com.sda.and1;
+
+import java.util.Scanner;
+
+public class Exercies1 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        String[] splitArray= inputString.split(",");
+        Integer.valueOf("1");
+        //  int[]inputArr = {0, 1, 2, 3, 4, 5,6};
+        int[]inputArr = new int[splitArray.length];
+        for (int i = 0; i < splitArray.length; i++) {
+            inputArr[i]=Integer.valueOf(splitArray[i]);
+
+        }
+
+        printOdd(inputArr);
+    }
+    private static void printOdd(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0){
+                System.out.println(array[i]);
+            }
+
+        }
+    }
+}
