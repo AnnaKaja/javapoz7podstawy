01615f4def102c6b0e93f83d7383616f36a8b81e
anna kaja <anna_kaja@op.pl>
2018-03-20 22:11:56 +0100

    complete_tree

---
+++ src/complete_tree/Main.java
@@ -0,0 +1,41 @@
+package complete_tree;
+
+public class Main {
+
+    public static void main(String[] args){
+        CompleteTree completeTree = new CompleteTree(12);
+        Node root = new Node("A");
+        completeTree.add(root);
+        completeTree.add(new Node("B"));
+        completeTree.add(new Node("C"));
+        Node nodeD = new Node("D");
+        completeTree.add(nodeD);
+
+        completeTree.add(new Node("E"));
+        Node nodeF = new Node("F");
+        completeTree.add(nodeF);
+        completeTree.add(new Node("G"));
+        completeTree.add(new Node("H"));
+        completeTree.add(new Node("I"));
+        completeTree.add(new Node("J"));
+        completeTree.add(new Node("K"));
+        completeTree.add(new Node("L"));
+
+        printPreOrder(completeTree, root);
+
+        System.out.println();
+        completeTree.printInOrderRecurse(root);
+        System.out.println();
+        completeTree.printInOrder();
+    }
+
+    private static void printPreOrder(CompleteTree completeTree, Node node){
+        System.out.print(node.toString() + " ");
+        if(completeTree.getLeft(node) != null){
+            printPreOrder(completeTree, completeTree.getLeft(node));
+        }
+        if(completeTree.getRight(node) != null){
+            printPreOrder(completeTree, completeTree.getRight(node));
+        }
+    }
+}
