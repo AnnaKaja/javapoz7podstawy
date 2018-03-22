b011716db73930eb6b81002af90029ebf5659555
anna kaja <anna_kaja@op.pl>
2018-03-22 18:06:12 +0100

    insert_sort

---
+++ src/insert_sort/InsertSort.java
@@ -0,0 +1,28 @@
+package insert_sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        Integer[]array = {8,5,4,9,2,6};
+        insertSort (array);
+        System.out.println(Arrays.asList(array));
+    }
+
+    private static void insertSort(Integer[] array) {
+
+        int pointer = 1;
+        for (int i = 1; i < array.length ; i++) {
+            int val = array[i];
+            for (int j = i; j > 0; j--) {
+                if (array[j] > array[j - 1]) {
+                    int temp = array[j -1];
+                    array[j -1] = array[j];
+                    array[j] = temp;
+            }
+
+            }
+
+        }
+    }
+}
