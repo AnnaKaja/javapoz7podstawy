2d34e0c69c0b3913ab673243d0bff4f7b722ef40
anna kaja <anna_kaja@op.pl>
2018-03-22 15:48:52 +0100

    fixes

--- src/bubble_sort/BubbleSort.java
+++ src/bubble_sort/BubbleSort.java
@@ -9,17 +9,33 @@ public class BubbleSort {
         System.out.println(Arrays.asList(array));
     }
 
-    public static void bubbleSort(Integer[] array) {
-        for (int j = 0; j< array.length -1; j++) {
-            if (array[j] > array[j +1]){
-                int temp = array[j +1];
-                array[j + 1] = array[j];
-                array[j] = temp;
-                bubbleSort(array);
-
-            }
+    private static void bubbleSort(Integer[] array) {
+        boolean isSorted = true;
+        for (int i = 0; i < array.length ; i++) {
+            for (int j = 0; j < array.length -1 ; j++) {
+                if (array[j] > array[j +1]){
+                    isSorted = false;
+                    int temp = array[j +1];
+                    array[j +1] = array[j];
+                    array[j] = temp;
+                  }
+                }
+                if(isSorted){
+                break;
+                }
+                isSorted = true;
 
         }
+    //   for (int j = 0; j< array.length -1; j++) {
+    //   if (array[j] > array[j +1]){
+    //            int temp = array[j +1];
+    //            array[j + 1] = array[j];
+    //            array[j] = temp;
+    //            bubbleSort(array);
+
+    //        }
+
+    //   }
 
     }
 }
