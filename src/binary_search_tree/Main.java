304cbd913ac9828e552e74c8ad444fc8b1c96728
anna kaja <anna_kaja@op.pl>
2018-04-04 19:48:01 +0200

    bst

---
+++ src/binary_search_tree/Main.java
@@ -0,0 +1,9 @@
+package binary_search_tree;
+
+public class Main {
+    public static void main(String[] args) {
+        MyBst myBst = new MyBst();
+        int[] input = {1, 2, 3, 4, 5, 6, 7};
+        myBst.add(input);
+    }
+}
